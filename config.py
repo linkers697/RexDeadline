45d77de3bc64d685ebd63e5d6e3c7594404041b0
linkers697 <music3fhhgyhfgh@gmail.com>
2025-10-05 21:08:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 #Get API_KEY from @DeadlineTechOwner or @DeadlineApiBot
-API_URL = getenv("API_URL", "https://deadlinetech.site")
-API_KEY = getenv("API_KEY", "vDILK66511myciVLMXwS")
+API_URL = getenv("API_URL", 'https://deadlinetech.site')
+API_KEY = getenv("API_KEY", 'vDILK66511myciVLMXwS')
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
