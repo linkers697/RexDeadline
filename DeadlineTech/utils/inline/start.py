faf8779ddfae6a51ac0ea893023097f49ce9b012
lexer697 <music1ygffygfgdf@gmail.com>
2025-10-10 22:24:22 +0530

    Update start.py

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -41,7 +41,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL), 
-            InlineKeyboardButton(text=_["S_B_5"], url="https://t.me/AuraVisual")
+            InlineKeyboardButton(text=_["S_B_5"], url="https://t.me/TryAura")
         ]
     ]
     return buttons
